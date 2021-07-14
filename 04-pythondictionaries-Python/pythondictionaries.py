4d092d31bcefefcfbd33126c90abe1bd44908f6f
Anusha-360 <anushathodupunoori123@msitprogram.net>
2021-07-14 12:11:48 +0530

    done

---
+++ 04-pythondictionaries-Python/pythondictionaries.py
@@ -0,0 +1,58 @@
+"""Time to play with Python dictionaries!
+You're going to work on a dictionary that
+stores cities by country and continent.
+One is done for you - the city of Mountain 
+View is in the USA, which is in North America.
+
+You need to add the cities listed below by
+modifying the structure.
+Then, you should print out the values specified
+by looking them up in the structure.
+
+Cities to add:
+Bangalore (India, Asia)
+Atlanta (USA, North America)
+Cairo (Egypt, Africa)
+Shanghai (China, Asia)"""
+
+
+"""Print the following (using "print").
+1. A list of all cities in the USA in
+alphabetic order. Make it function with name as sortUSA(), return list of cities
+2. All cities in Asia, in alphabetic
+order, next to the name of the country.
+In your output, label each answer with a number
+so it looks like this: (Make it function with name as alphaAsia(), return list of cities)
+1
+American City
+American City
+2
+Asian City - Country
+Asian City - Country"""
+
+
+def sortUSA():
+    '''Return all the cities in the USA in alphabetical order'''
+    list = locations['North America']['USA']
+    list.sort()
+    return list
+
+def alphaAsia():
+    '''Return all the cities in Asia continent in alphabetical order'''
+    list1=locations['Asia']
+    print(list1)
+    s = ''
+    res = []
+    for i in list1:
+        s =str(list1[i][0]) + '-' + str(i)
+        res.append(s)
+        s = ''
+        res.sort()
+    return res    
+
+# Note: Check for test cases to understand the output format.
+locations = {'North America': {'USA': ['Mountain View']}}
+locations['Asia']={'china':['Shanghai']}
+locations['Africa']={'Egypt':['cairo']}
+locations['Asia']['India']=['Bangalore']
+locations['North America']['USA'].append('Atlanta')
