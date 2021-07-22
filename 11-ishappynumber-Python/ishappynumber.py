f6346ed89ad843e7ab6b0dfa2059fbea8b96e75d
Anusha-360 <anushathodupunoori123@msitprogram.net>
2021-07-22 12:09:13 +0530

    done

--- 11-ishappynumber-Python/ishappynumber.py
+++ 11-ishappynumber-Python/ishappynumber.py
@@ -18,5 +18,16 @@
 # assert(ishappynumber(405) == False)
 
 def ishappynumber(n):
-	# your code goes here
-	pass
+    sum = 0
+    while(n!=0):
+        sum += (n%10)**2
+        n//=10
+    if sum == 1:
+        return True
+    elif sum<10:
+        return False
+    else:
+        return ishappynumber(sum)
+    
+k = ishappynumber(-7)
+print(k)    
