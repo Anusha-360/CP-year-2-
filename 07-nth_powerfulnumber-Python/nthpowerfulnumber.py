974a825a1dce37f66a3f8fd222ba562bafdc65e2
Anusha-360 <anushathodupunoori123@msitprogram.net>
2021-07-28 10:20:45 +0530

    done

--- 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
+++ 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
@@ -15,24 +15,55 @@ def nthpowerfulnumber(n):
         if(isPowerful(g)):
             f += 1
     return g
-
+ 
+# function to check if
+# the number is powerful
 def isPowerful(n):
+ 
+    # First divide the number repeatedly by 2
     while (n % 2 == 0):
-        l = 0
+ 
+        power = 0
         while (n % 2 == 0):
-            n = n // 2
-            l = l + 1
-        if ( l == 1):
+         
+            n = n//2
+            power = power + 1
+         
+          
+        # If only 2 ^ 1 divides
+        # n (not higher powers),
+        # then return false
+        if (power == 1):
             return False
-    for power in range(3, int(math.sqrt(n))+1, 2):
-        l = 0
-        while (n % power == 0):
-            n = n // power
-            l = l + 1
-        if (l == 1):
+     
+  
+    # if n is not a power of 2
+    # then this loop will execute
+    # repeat above process
+    for factor in range(3, int(math.sqrt(n))+1, 2):
+     
+        # Find highest power of
+        # "factor" that divides n
+        power = 0
+        while (n % factor == 0):
+         
+            n = n//factor
+            power = power + 1
+         
+  
+        # If only factor ^ 1 divides
+        # n (not higher powers),
+        # then return false
+        if (power == 1):
             return False
+     
+  
+     # n must be 1 now if it
+     # is not a prime numenr.
+     # Since prime numbers are
+     # not powerful, we return
+     # false if n is not 1.
     return (n == 1)
-
 
 
 #print(nthPowerfulNumber(int(input())))
